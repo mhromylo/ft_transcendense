6f9b2681abe70a3a0123f4e56a099e275922e394
mhromylo <142239126+mhromylo@users.noreply.github.com>
2024-12-03 18:33:23 +0100

    Delete ft_transcendence directory

--- ft_transcendence/game/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Player
-
-class PlayerProfileForm(forms.ModelForm):
-    class Meta:
-        model = Player
-        fields = ['display_name', 'avatar']
-
